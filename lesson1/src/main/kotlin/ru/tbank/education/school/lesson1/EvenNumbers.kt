c9be3e741e57db126f469e73605b33484146916f
evivuri <evivuri@gmail.com>
2025-11-09 12:25:24 +0300

    sum of even numbers method

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -4,6 +4,7 @@ package ru.tbank.education.school.lesson1
  * Сумма четных чисел.
  */
 fun sumEvenNumbers(numbers: Array<Int>): Int {
+    if (numbers.isNullOrEmpty()) return 0
     var res = 0
     for (num in numbers) {
         if (num % 2 == 0) res += num
