e09ea71cfdc4a63bd0cc51b71791121c72bd1dea
evivuri <evivuri@gmail.com>
2025-11-12 17:10:55 +0300

    calculator method

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -4,7 +4,12 @@ package ru.tbank.education.school.lesson1
  * Метод для вычисления простых арифметических операций.
  */
 fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+    return when (operation) {
+        OperationType.ADD -> a + b
+        OperationType.SUBTRACT -> a - b
+        OperationType.MULTIPLY -> a * b
+        OperationType.DIVIDE -> if (b == 0.0) null else a / b
+    }
 }
 
 /**
@@ -14,5 +19,17 @@ fun calculate(a: Double, b: Double, operation: OperationType): Double? {
  */
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val parts = this.split(" ");
+    if (parts.size != 3) return null
+    val a = parts[0].toDoubleOrNull() ?: return null
+    val b = parts[2].toDoubleOrNull() ?: return null
+
+    val op = when (parts[1]) {
+        "+" -> OperationType.ADD
+        "-" -> OperationType.SUBTRACT
+        "*" -> OperationType.MULTIPLY
+        "/" -> OperationType.DIVIDE
+        else -> return null
+    }
+    return calculate(a, b, op)
 }
