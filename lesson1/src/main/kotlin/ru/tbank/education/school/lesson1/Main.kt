118ed8112fcfe42f7bb99a8a3deefb93c07eddf5
evivuri <evivuri@gmail.com>
2025-11-13 11:02:23 +0300

    calculator method

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,5 +1,15 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
-    print("I love C++")
+    val a = readLine()
+    if (a.isNullOrEmpty()) {
+        print("Error\n")
+        return
+    }
+    val res = a.calculate()
+    res?.let {
+        println("Result: $res")
+    } ?: println("Error")
 }
+
+
