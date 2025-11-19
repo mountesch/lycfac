9b4c7688b50f18dee867cd0eccc59c091fd6c4d6
evivuri <evivuri@gmail.com>
2025-11-19 19:38:45 +0300

    User class has been added

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/music/User.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/music/User.kt
@@ -4,9 +4,28 @@ class User (
     val name: String,
     private val playlist: Playlist
 ) {
+
+    // Managing user's playlist
     fun addToPlaylist(item: AudioItem) = playlist.addItem(item)
     fun showPlaylist(playlist: Playlist) {
         println("User: $name")
         playlist.printInfo()
+    }
+
+    // Managing user's subscription
+    private var subscriptionStatus: Subscription? = null
+    fun subscribe(type: Subscription) {
+        subscriptionStatus = type
+        println("You've subscribed to the ${type.name} plan")
+    }
+
+    fun unsubscribe() {
+        subscriptionStatus = null
+        println("You've been unsubscribed")
+    }
+
+    fun getSubscriptionStatus() {
+        return (subscriptionStatus?.let { "Currently subscribed to ${it.name} plan" }
+            ?: "$name has no subscription") as Unit
     }
 }
