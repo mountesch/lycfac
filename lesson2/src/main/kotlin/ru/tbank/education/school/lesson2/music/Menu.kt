45639b646b108dbf336f11eab6d2164dd0154dfd
evivuri <evivuri@gmail.com>
2025-11-19 20:16:36 +0300

    Function for printing the menu (used in Main.kt)

---
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/music/Menu.kt
@@ -0,0 +1,11 @@
+package ru.tbank.education.school.lesson2.music
+
+fun printMenu() {
+    println("""Choose a section to proceed:
+1 - My Profile and subscription
+2 - Manage my favourite playlist
+3 - Manage albums
+4 - Manage playlists
+5 - Show the state of the player
+exit - leave the service"""")
+}
