833fb7e174f04085865053014f65d93546f9ffaf
evivuri <evivuri@gmail.com>
2025-11-19 18:30:22 +0300

    Abstract class of audio items added

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/music/AudioItem.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/music/AudioItem.kt
@@ -4,5 +4,5 @@ abstract class AudioItem( // Abstract class of any audio items
     val title: String,
     protected val durationSec: Int
 ) {
-    open fun info(): String = "$title: ($durationSec)s" // get the information about this item
+    open fun info(): String = "$title: ($durationSec)s" // Get the information about this item
 }
