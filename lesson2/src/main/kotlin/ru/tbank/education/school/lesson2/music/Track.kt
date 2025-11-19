d11e2029aff68435ebae65c7f9c43a9aa350175a
evivuri <evivuri@gmail.com>
2025-11-19 18:41:55 +0300

    Class of tracks added

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/music/Track.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/music/Track.kt
@@ -1,4 +1,17 @@
 package ru.tbank.education.school.lesson2.music
 
-class Track {
+class Track (
+    title: String,
+    durationSec: Int,
+    private var artist: String
+) : AudioItem(title, durationSec) {
+    constructor(title: String, durationSec: Int) :
+            this(title, durationSec, "Unknown artist")
+
+    val fullTitle: String // Getting full title
+        get() = "$artist - $title"
+
+    override fun info(): String { // Returns full information about this track
+        return "Track: $fullTitle ${durationSec}s"
+    }
 }
